6b6045ccb493177446f5ac6c6dc49a0e9b1cb8d4
Дамир <damir.obuhov@mail.ru>
2022-11-02 22:46:15 +0400

    ЗАдача 8

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int clientOs = 0;
+        int clientDeviceYear = 2013;
+        if (clientOs == 0) {
+            if (clientDeviceYear > 2014) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }   else {
+            if (clientDeviceYear > 2014) {
+                System.out.println("Установите  версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+    }
+}
